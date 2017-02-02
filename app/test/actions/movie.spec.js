354cf86bcffbd74e7be821c181c58d19e0dc61f5
Dylan Avery <fossilfuelmusic@gmail.com>
2017-02-01 18:54:24 -0700

    all the dev depends, setting up test suite

---
+++ app/test/actions/movie.spec.js
@@ -0,0 +1,10 @@
+import * as actions from '../../actions/index'
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import { expect } from 'chai';
+
+describe('actions', () => {
+  it('should create an action to add a todo', () => {
+    expect(true).toEqual(true)
+  })
+})
--- app/test/actions/movie.test.js
+++
@@ -1,7 +0,0 @@
-import * as actions from '../../app/actions'
-
-describe('actions', () => {
-  it('should create an action to add a todo', () => {
-    expect(true).toEqual(true)
-  })
-})
