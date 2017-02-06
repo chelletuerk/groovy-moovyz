3c160263afbdb82d9642cec3a1c0d7bb8d104ba5
Dylan Avery <fossilfuelmusic@gmail.com>
2017-02-05 21:13:57 -0700

    made the thing into a link, changed the display to block on the links

--- app/components/App.js
+++ app/components/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router'
 
 export default class App extends Component {
+
+  // home() {
+  //   return browserHistory.push('/')
+  // }
+
   render() {
     return (
       <div>
-        <h1 className='header-title'>Groovie Movies</h1>
+        <Link id="tite" to="/"><h1 className='header-title'>Groovie Movies</h1></Link>
         {this.props.children}
       </div>
     )
